c697909c45d4f8eaf2198f7f580c2b790f47d4fb
G-shop <n8900664@gmail.com>
2024-04-26 17:00:41 +0600

    Initial commit

---
+++ app/src/main/java/com/example/home_work_5_2/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.home_work_5_2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.edit_text);
+        textView = findViewById(R.id.text_viev);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                textView.setText(text);
+            }
+
+        });
+        ;
+
+    }
+}
